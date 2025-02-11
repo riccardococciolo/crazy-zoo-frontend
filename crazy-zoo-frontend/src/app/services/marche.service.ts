63aea8916bbb61ff774ff6d93e598756ec98be9d
AndreaGiga00 <59709507+AndreaGiga00@users.noreply.github.com>
2025-02-11 11:21:51 +0100

    Create constants.ts

--- crazy-zoo-frontend/src/app/services/marche.service.ts
+++ crazy-zoo-frontend/src/app/services/marche.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { CONSTANTS } from '../shared/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarcheService {
+  COSTANTE = CONSTANTS.API_URL 
 
   constructor() { }
 }
---
+++ crazy-zoo-frontend/src/app/shared/constants.ts
@@ -0,0 +1,5 @@
+export const CONSTANTS = {
+    API_URL: 'https://api.example.com',
+    APP_VERSION: '1.0.0'
+  };
+  
