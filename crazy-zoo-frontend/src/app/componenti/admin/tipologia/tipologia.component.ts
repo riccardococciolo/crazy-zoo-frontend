a0533d30b3ffdcb0b713d53cad3e7ddb3ee7ba45
Giobbs <luc.florio@libero.it>
2025-02-14 15:37:50 +0100

    tipo

--- crazy-zoo-frontend/src/app/componenti/admin/tipologia/tipologia.component.ts
+++ crazy-zoo-frontend/src/app/componenti/admin/tipologia/tipologia.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TipologieService } from '../../../services/tipologie.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tipologia',
@@ -8,4 +11,39 @@ import { Component } from '@angular/core';
 })
 export class TipologiaComponent {
 
+  data: any;
+  response: any;
+  msg: string = '';
+  constructor(
+    private serv: TipologieService,
+    private routing: Router,
+    private route: ActivatedRoute
+    ) {}
+  
+    ngOnInit(): void {
+      console.log('ngOnInit');
+      this.serv.getTipologia().subscribe((resp: any) => {
+        this.response = resp;
+        this.data = this.response.dati;
+      });
+    }
+  
+    deleteAction(id: number) {
+      console.log(id);
+      let formData = new FormData();
+      formData.append('id', id.toString());
+      console.log(formData);
+  
+      this.serv.deleteTipologia(formData).subscribe((resp: any) => {
+        if (resp.rc) {
+          console.log(resp.rc);
+  
+          this.routing.navigate(['/admin/tipologia']).then(() => {
+            window.location.reload();
+          });
+        } else {
+          this.msg = resp.msg;
+        }
+      });
+    }
 }
