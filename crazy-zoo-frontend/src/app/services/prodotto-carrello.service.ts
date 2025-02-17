c6a1b2a9ba5c65e54c6b55b104ea995f2de37def
AndreaGiga00 <59709507+AndreaGiga00@users.noreply.github.com>
2025-02-17 11:41:46 +0100

    prodotto card

--- crazy-zoo-frontend/src/app/services/prodotto-carrello.service.ts
+++ crazy-zoo-frontend/src/app/services/prodotto-carrello.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CONSTANTS } from '../shared/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdottoCarrelloService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  addProdottoToCarrello(body:{}){
+    return this.http.post(CONSTANTS.API_URL + "prodcarr/addprodottotocarrello",body)
+  }
 }
