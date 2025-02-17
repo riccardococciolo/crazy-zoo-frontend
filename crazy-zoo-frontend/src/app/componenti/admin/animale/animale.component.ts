d4e4e76c266339c8e949a94d881a73122998a36e
Maniaf <manuel.iafano76@gmail.com>
2025-02-17 10:49:20 +0100

    inizio animale

--- crazy-zoo-frontend/src/app/componenti/admin/animale/animale.component.ts
+++ crazy-zoo-frontend/src/app/componenti/admin/animale/animale.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimaliService } from '../../../services/animali.service';
 
 @Component({
   selector: 'app-animale',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './animale.component.css'
 })
 export class AnimaleComponent {
+  listAnimali: any;
+  response: any;
+  msg: string = '';
+  constructor(private animaleSer: AnimaliService) {}
 
+  ngOnInit(): void {
+    console.log("caricamento dati..");
+    this.animaleSer.getAnimale().subscribe((resp: any) => { 
+      this.response = resp;
+      this.listAnimali = this.response.dati;
+    });
+  }
+
+  deleteAction(id: number) {
+    console.log("Delete premuto " + id);
+    this.animaleSer.deleteAnimale({id: id}).subscribe((response: any) => {
+      if (response.rc) {
+        alert("Animale eliminato con successo!");
+      }
+    });
+}
 }
--- crazy-zoo-frontend/src/app/componenti/admin/create-animale/create-animale.component.ts
+++ crazy-zoo-frontend/src/app/componenti/admin/create-animale/create-animale.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimaliService } from '../../../services/animali.service';
 
 @Component({
   selector: 'app-create-animale',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './create-animale.component.css'
 })
 export class CreateAnimaleComponent {
-
+  
 }
