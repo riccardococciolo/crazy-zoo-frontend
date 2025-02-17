032a56b616a80f9ea2b19eaa0a1e41bf95450ab6
AndreaGiga00 <59709507+AndreaGiga00@users.noreply.github.com>
2025-02-17 12:40:03 +0100

    first dettaglio-pro

--- crazy-zoo-frontend/src/app/componenti/dettaglio-prodotto/dettaglio-prodotto.component.ts
+++ crazy-zoo-frontend/src/app/componenti/dettaglio-prodotto/dettaglio-prodotto.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProdottiService } from '../../services/prodotti.service';
 
 @Component({
   selector: 'app-dettaglio-prodotto',
@@ -6,6 +8,51 @@ import { Component } from '@angular/core';
   templateUrl: './dettaglio-prodotto.component.html',
   styleUrl: './dettaglio-prodotto.component.css'
 })
-export class DettaglioProdottoComponent {
+export class DettaglioProdottoComponent implements OnInit{
+  
+  constructor(private prodS: ProdottiService, private router:Router,private route:ActivatedRoute){
 
+  }
+  id:any
+ infoProd:any
+  ngOnInit(): void {
+    this.loadProduct()
+
+
+  }
+
+  loadProduct(){
+    this.id= Number(this.route.snapshot.paramMap.get("id"))
+    
+    this.prodS.getProdotto({id : this.id}).subscribe((resp:any)=>{
+      if(resp.rc){
+      this.infoProd =resp.dati
+      console.log(this.infoProd)
+    }else{
+      alert("Errore")
+    }
+    })
+
+
+    //immagini
+    const base64Data = this.infoProd.immagini.data;
+    const contentType = 'image/jpeg'; 
+    const blob = this.base64ToBlob(base64Data, contentType);
+    console.log(blob)
+    const file = new File([blob], "nomefile.ext", { type: blob.type });
+    console.log(file)
+    const imageUrl = URL.createObjectURL(blob);
+  }
+  base64ToBlob(base64: string, contentType: string): Blob {
+    const byteCharacters = atob(base64);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], { type: contentType });
+  }
+
+
+    
 }
--- crazy-zoo-frontend/src/app/services/recensioni.service.ts
+++ crazy-zoo-frontend/src/app/services/recensioni.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CONSTANTS } from '../shared/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecensioniService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  getRecensioniByProdotto(id:any){
+    return this.http.get(CONSTANTS.API_URL + "recensioni/listbyprodotto",id)
+  }
 }
