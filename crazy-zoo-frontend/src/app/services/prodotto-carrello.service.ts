3bdf540710022c5b0b5c011493092dde4351884d
DarioZancuo <dario.zancuo@hotmail.it>
2025-02-20 17:12:09 +0100

    prodotto/carrello

--- crazy-zoo-frontend/src/app/services/prodotto-carrello.service.ts
+++ crazy-zoo-frontend/src/app/services/prodotto-carrello.service.ts
@@ -1,14 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CONSTANTS } from '../shared/constants';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdottoCarrelloService {
 
+  private carrelloAggiornato = new BehaviorSubject<boolean>(false);
+  carrelloAggiornato$ = this.carrelloAggiornato.asObservable();
+
   constructor(private http: HttpClient) { }
-  addProdottoToCarrello(body:{}){
-    return this.http.post(CONSTANTS.API_URL + "prodcarr/addprodottotocarrello",body)
+
+  // Metodo per notificare il carrello dell'aggiornamento
+  aggiornaCarrello() {
+    this.carrelloAggiornato.next(true);
   }
+
+  // Metodo per aggiungere un prodotto al carrello
+  addProdottoToCarrello(body: {}) {
+    return this.http.post(CONSTANTS.API_URL + "prodcarr/addprodottotocarrello", body).pipe(
+      tap(() => {
+        this.aggiornaCarrello(); // 🔥 Notifica il carrello dopo aver aggiunto un prodotto
+      })
+    );
+  }
+
+
+
+
+
 }
