31e4ffa1e1713ddb5ffa7640fe0ca17c7c292d00
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2025-02-20 22:38:35 +0100

    home

--- crazy-zoo-frontend/src/app/componenti/home/home.component.ts
+++ crazy-zoo-frontend/src/app/componenti/home/home.component.ts
@@ -6,23 +6,6 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent{
 
-  @ViewChild('heroOverlay') heroOverlay!: ElementRef;
-  @ViewChild('homeContent') homeContent!: ElementRef;
-
-  constructor() {}
-
-  ngAfterViewInit(): void {
-    if (!this.heroOverlay || !this.homeContent) {
-      console.error("Elementi non trovati!");
-    }
-  }
-
-  closeHeroPopup(): void {
-    if (this.heroOverlay && this.homeContent) {
-      this.heroOverlay.nativeElement.classList.add("hidden");
-      this.homeContent.nativeElement.classList.add("show");
-    }
-  }
 }
