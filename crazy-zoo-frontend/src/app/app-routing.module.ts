b444c6f10912e599441ad65371d02b4cdfe26f91
Giobbs <luc.florio@libero.it>
2025-02-27 16:20:57 +0100

    graf

--- crazy-zoo-frontend/src/app/app-routing.module.ts
+++ crazy-zoo-frontend/src/app/app-routing.module.ts
@@ -1,34 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './componenti/home/home.component';
-import { DettaglioProdottoComponent } from './componenti/dettaglio-prodotto/dettaglio-prodotto.component';
-import { DettaglioUtenteComponent } from './componenti/dettaglio-utente/dettaglio-utente.component';
-import { AnimaleComponent } from './componenti/admin/animale/animale.component';
-import { UpdateAnimaleComponent } from './componenti/admin/update-animale/update-animale.component';
-import { CreateAnimaleComponent } from './componenti/admin/create-animale/create-animale.component';
-import { MarcaComponent } from './componenti/admin/marca/marca.component';
-import { UpdateMarcaComponent } from './componenti/admin/update-marca/update-marca.component';
-import { CreateMarcaComponent } from './componenti/admin/create-marca/create-marca.component';
-import { ProdottoComponent } from './componenti/admin/prodotto/prodotto.component';
-import { ProdottiComponent } from './componenti/prodotti/prodotti.component';
-import { UpdateProdottoComponent } from './componenti/admin/update-prodotto/update-prodotto.component';
-import { CreateProdottoComponent } from './componenti/admin/create-prodotto/create-prodotto.component';
-import { UpdateTipologiaComponent } from './componenti/admin/update-tipologia/update-tipologia.component';
-import { CreateTipologiaComponent } from './componenti/admin/create-tipologia/create-tipologia.component';
-import { TipologiaComponent } from './componenti/admin/tipologia/tipologia.component';
-import { LoginComponent } from './componenti/login/login.component';
-import { RegisterComponent } from './componenti/register/register.component';
 import { AdminGuard } from './auth/admin.guard';
 import { AuthGuard } from './auth/auth.guard';
-import { OrdineComponent } from './componenti/ordine/ordine.component';
-import { OrdineSuccesComponent } from './componenti/ordine-succes/ordine-succes.component';
+import { AnimaleComponent } from './componenti/admin/animale/animale.component';
+import { CreateAnimaleComponent } from './componenti/admin/create-animale/create-animale.component';
+import { CreateMarcaComponent } from './componenti/admin/create-marca/create-marca.component';
+import { CreateProdottoComponent } from './componenti/admin/create-prodotto/create-prodotto.component';
+import { CreateTipologiaComponent } from './componenti/admin/create-tipologia/create-tipologia.component';
+import { MarcaComponent } from './componenti/admin/marca/marca.component';
+import { ProdottoComponent } from './componenti/admin/prodotto/prodotto.component';
+import { TipologiaComponent } from './componenti/admin/tipologia/tipologia.component';
+import { UpdateAnimaleComponent } from './componenti/admin/update-animale/update-animale.component';
+import { UpdateMarcaComponent } from './componenti/admin/update-marca/update-marca.component';
+import { UpdateProdottoComponent } from './componenti/admin/update-prodotto/update-prodotto.component';
+import { UpdateTipologiaComponent } from './componenti/admin/update-tipologia/update-tipologia.component';
+import { DettaglioProdottoComponent } from './componenti/dettaglio-prodotto/dettaglio-prodotto.component';
+import { DettaglioUtenteComponent } from './componenti/dettaglio-utente/dettaglio-utente.component';
 import { ErrorComponent } from './componenti/error/error.component';
+import { HomeComponent } from './componenti/home/home.component';
+import { LoginComponent } from './componenti/login/login.component';
+import { OrdineSuccesComponent } from './componenti/ordine-succes/ordine-succes.component';
+import { OrdineComponent } from './componenti/ordine/ordine.component';
+import { PrivacyComponent } from './componenti/privacy/privacy.component';
+import { ProdottiComponent } from './componenti/prodotti/prodotti.component';
+import { RegisterComponent } from './componenti/register/register.component';
+import { TerminiComponent } from './componenti/termini/termini.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
 
+  { path: 'privacy', component: PrivacyComponent },
+  { path: 'termini', component: TerminiComponent },
 
   // Login e Registrazione
   { path: 'login', component: LoginComponent },
